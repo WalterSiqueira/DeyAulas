9f56fa1110d1701b31d975c939aceb3db9360c85
WalterSiqueira <walterbarbozasiqueira@gmail.com>
2024-01-02 15:09:46 -0300

    telas login e cadastro em andamento

---
+++ Views/Cadastro/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>DeyAulas - cadastro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../../Public/Cadastro/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <header class="header_cadastro">
+      <div class="div_logo">
+        <a class="link_logo" href="../../index.php"><img class="logo_img" src="../../Public/Img/logo.png"></a>
+      </div>
+      <nav>
+        <div class="mobile_icones">
+          <img class="menu_img" src="../../Public/Img/icone_menu_mobile.png">
+        </div>
+        <div class="desktop_icones">
+          <a class="link_catalogo" href="../Cadastro"><img class="catalogo_img" src="../../Public/Img/icone_catalogo.png"></a>
+          <a class="link_login" href="../Login"><img  class="login_img" src="../../Public/Img/icone_login.png"></a>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="div_form">
+        <h2>Cadastro</h2>
+        <form action="#" method="post">
+          <label>E-mail:</label>
+          <input type="email">
+          <label>Senha:</label>
+          <input type="password">
+          <button>Cadastrar</button>
+        </form>
+      </div>
+    </main> 
+  </body>
+</html>
---
+++ Views/Login/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>DeyAulas - Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../../Public/Login/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <header class="header_login">
+      <div class="div_logo">
+        <a class="link_logo" href="../../index.php"><img class="logo_img" src="../../Public/Img/logo.png"></a>
+      </div>
+      <nav>
+        <div class="mobile_icones">
+          <img class="menu_img" src="../../Public/Img/icone_menu_mobile.png">
+        </div>
+        <div class="desktop_icones">
+          <a class="link_catalogo" href="../Cadastro"><img class="catalogo_img" src="../../Public/Img/icone_catalogo.png"></a>
+          <a class="link_cadastro" href="../Cadastro"><img  class="cadastro_img" src="../../Public/Img/icone_cadastro.png"></a>
+        </div>
+    </header>
+    <main>
+      <div class="div_form">
+        <h2>Login</h2>
+        <form action="#" method="post">
+          <label>E-mail:</label>
+          <input type="email">
+          <label>Senha:</label>
+          <input type="password">
+          <h3>Esqueci minha senha</h3>
+          <button>Entrar</button>
+        </form>
+      </div>
+    </main> 
+  </body>
+</html>
