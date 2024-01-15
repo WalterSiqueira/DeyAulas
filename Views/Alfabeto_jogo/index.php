2f6e93817d3d00a586720d14be140be4aca5f4cd
WalterSiqueira <walterbarbozasiqueira@gmail.com>
2024-01-15 18:12:26 -0300

    jogo alfabeto - começando

---
+++ Views/Alfabeto_jogo/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <main>
+            <div class="alfabeto">
+                <img class="letra-a" src="../../Public/Img/img_jogo_alfabeto/letra-a.png"/>
+                <img class="letra-b"  src="../../Public/Img/img_jogo_alfabeto/letra-b.png"/>
+                <img class="letra-c"  src="../../Public/Img/img_jogo_alfabeto/letra-c.png"/>
+                <img class="letra-d"  src="../../Public/Img/img_jogo_alfabeto/letra-d.png"/>
+                <img class="letra-e"  src="../../Public/Img/img_jogo_alfabeto/letra-e.png"/>
+                <img class="letra-f"  src="../../Public/Img/img_jogo_alfabeto/letra-f.png"/>
+                <img class="letra-g"  src="../../Public/Img/img_jogo_alfabeto/letra-g.png"/>
+                <img class="letra-h"  src="../../Public/Img/img_jogo_alfabeto/letra-h.png"/>
+                <img class="letra-i"  src="../../Public/Img/img_jogo_alfabeto/letra-i.png"/>
+                <img class="letra-j"  src="../../Public/Img/img_jogo_alfabeto/letra-j.png"/>
+                <img class="letra-k"  src="../../Public/Img/img_jogo_alfabeto/letra-k.png"/>
+                <img class="letra-l"  src="../../Public/Img/img_jogo_alfabeto/letra-l.png"/>
+                <img class="letra-m"  src="../../Public/Img/img_jogo_alfabeto/letra-m.png"/>
+                <img class="letra-n"  src="../../Public/Img/img_jogo_alfabeto/letra-n.png"/>
+                <img class="letra-o"  src="../../Public/Img/img_jogo_alfabeto/letra-o.png"/>
+                <img class="letra-p"  src="../../Public/Img/img_jogo_alfabeto/letra-p.png"/>
+                <img class="letra-q"  src="../../Public/Img/img_jogo_alfabeto/letra-q.png"/>
+                <img class="letra-r"  src="../../Public/Img/img_jogo_alfabeto/letra-r.png"/>
+                <img class="letra-s"  src="../../Public/Img/img_jogo_alfabeto/letra-s.png"/>
+                <img class="letra-t"  src="../../Public/Img/img_jogo_alfabeto/letra-t.png"/>
+                <img class="letra-u"  src="../../Public/Img/img_jogo_alfabeto/letra-u.png"/>
+                <img class="letra-v"  src="../../Public/Img/img_jogo_alfabeto/letra-v.png"/>
+                <img class="letra-w"  src="../../Public/Img/img_jogo_alfabeto/letra-w.png"/>
+                <img class="letra-x"  src="../../Public/Img/img_jogo_alfabeto/letra-x.png"/>
+                <img class="letra-y"  src="../../Public/Img/img_jogo_alfabeto/letra-y.png"/>
+                <img class="letra-z"  src="../../Public/Img/img_jogo_alfabeto/letra-z.png"/>
+            </div>
+        </main>
+    </body>
+</html>
