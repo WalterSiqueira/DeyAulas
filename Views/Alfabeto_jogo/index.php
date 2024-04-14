0b42f2b35ca88dbe449a47c9e852b5fea01d5836
WalterSiqueira <walterbarbozasiqueira@gmail.com>
2024-04-14 19:08:14 -0300

    jogo alfabeto com design novo

--- Views/Alfabeto_jogo/index.php
+++ Views/Alfabeto_jogo/index.php
@@ -3,6 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Piedra&display=swap" rel="stylesheet">
   <title>Jogo do Alfabeto</title>
   <link rel="stylesheet" href="../../Public/Alfabeto_jogo/style.css">
 </head>
