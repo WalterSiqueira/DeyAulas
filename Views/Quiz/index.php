1f3c8e8e782ae57f0b4ded629647f7d852c280aa
WalterSiqueira <walterbarbozasiqueira@gmail.com>
2024-04-23 21:07:17 -0300

    Jogo Quiz adicionado

---
+++ Views/Quiz/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Tags Meta -->
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Javascript Duolingo">
+    <!-- Configuração do favicon -->
+    <link rel="icon" type="image/x-icon" href="favic.ico">
+    <!-- Título na guia do navegador -->
+    <title>Javascript Duolingo</title>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&family=Oswald&family=PT+Serif:wght@700&display=swap" rel="stylesheet">
+    <!-- Conexão da folha de estilos -->
+    <link rel="stylesheet" href="../../Public/Quiz/style.css">
+</head>
+<body>
+	<div id="wrapper">
+		<h3>Selecione a alternativa correta:</h3>
+		<div id="titulo"></div>
+		
+		<ul>
+			<li class="alternativa"></li>
+			<li class="alternativa"></li>
+			<li class="alternativa"></li>
+			<li class="alternativa"></li>
+		</ul>
+		<div id="result"></div>
+		<div id="pontos"></div>
+	</div>
+	<script type="text/javascript" src="../../Public/Quiz/script.js"></script>
+	
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
